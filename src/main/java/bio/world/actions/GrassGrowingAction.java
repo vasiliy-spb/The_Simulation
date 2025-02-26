958258ae619796d18793aecdb9f91cfa77c33695
vasiliy-spb <vasiliy-spb07@mail.ru>
2025-02-27 03:14:00 +0700

    Made a refactor of GrassGrowingAction class

--- src/main/java/bio/world/actions/GrassGrowingAction.java
+++ src/main/java/bio/world/actions/GrassGrowingAction.java
@@ -23,12 +23,12 @@ public class GrassGrowingAction implements Action {
 
     @Override
     public void perform() {
-        if (tickCounter.getCurrentTick() % 2 == 0) {
+        if (shouldSkipMove()) {
             return;
         }
 
         List<Coordinates> emptyCoordinates = createEmptyCoordinates();
-        if (emptyCoordinates.size() < 2) {
+        if (hasNotEnoughEmptyCells(emptyCoordinates)) {
             return;
         }
 
@@ -38,33 +38,8 @@ public class GrassGrowingAction implements Action {
         growGrass(countAdditionalGrass, emptyCoordinates);
     }
 
-    private void growGrass(int countAdditionalGrass, List<Coordinates> emptyCoordinates) {
-        for (int i = 0; i < countAdditionalGrass; i++) {
-            Coordinates coordinates = emptyCoordinates.get(i);
-            Grass grass = grassFactory.createInstanceBy(coordinates);
-            worldMap.addEntity(grass);
-        }
-    }
-
-    private int calculateCountAdditionalGrass(List<Coordinates> emptyCoordinates) {
-        int herbivoreGrassBalance = calculateHerbivoreGrassBalance();
-        int countAdditionalGrass = 1;
-        if (herbivoreGrassBalance > 0) {
-            countAdditionalGrass = Math.min(emptyCoordinates.size() / 2, herbivoreGrassBalance / 2);
-            countAdditionalGrass = Math.max(countAdditionalGrass, 1);
-        }
-        return countAdditionalGrass;
-    }
-
-    private int calculateHerbivoreGrassBalance() {
-        List<Entity> entities = worldMap.getAllEntities();
-        List<Entity> grassEntities = entities.stream()
-                .filter(e -> e instanceof Grass)
-                .toList();
-        List<Entity> herbivoreEntities = entities.stream()
-                .filter(e -> e instanceof Herbivore)
-                .toList();
-        return herbivoreEntities.size() - grassEntities.size();
+    private boolean shouldSkipMove() {
+        return tickCounter.getCurrentTick() % 2 == 0;
     }
 
     private List<Coordinates> createEmptyCoordinates() {
@@ -72,16 +47,58 @@ public class GrassGrowingAction implements Action {
         List<Coordinates> emptyCoordinates = new ArrayList<>();
         int height = worldMap.getHeight();
         int width = worldMap.getWidth();
+
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 Coordinates coordinates = new Coordinates(i, j);
+
                 if (busyCoordinates.contains(coordinates)) {
                     continue;
                 }
+
                 emptyCoordinates.add(coordinates);
                 busyCoordinates.add(coordinates);
             }
         }
+
         return emptyCoordinates;
+    }
+
+    private static boolean hasNotEnoughEmptyCells(List<Coordinates> emptyCoordinates) {
+        return emptyCoordinates.size() < 2;
+    }
+
+    private int calculateCountAdditionalGrass(List<Coordinates> emptyCoordinates) {
+        int herbivoreGrassBalance = calculateHerbivoreGrassBalance();
+        int countAdditionalGrass = 1;
+
+        if (herbivoreGrassBalance > 0) {
+            countAdditionalGrass = Math.min(emptyCoordinates.size() / 2, herbivoreGrassBalance / 2);
+            countAdditionalGrass = Math.max(countAdditionalGrass, 1);
+        }
+
+        return countAdditionalGrass;
+    }
+
+    private int calculateHerbivoreGrassBalance() {
+        List<Entity> entities = worldMap.getAllEntities();
+
+        List<Entity> grassEntities = entities.stream()
+                .filter(e -> e instanceof Grass)
+                .toList();
+
+        List<Entity> herbivoreEntities = entities.stream()
+                .filter(e -> e instanceof Herbivore)
+                .toList();
+
+        return herbivoreEntities.size() - grassEntities.size();
+    }
+
+    private void growGrass(int countAdditionalGrass, List<Coordinates> emptyCoordinates) {
+        for (int i = 0; i < countAdditionalGrass; i++) {
+            Coordinates coordinates = emptyCoordinates.get(i);
+            Grass grass = grassFactory.createInstanceBy(coordinates);
+            worldMap.addEntity(grass);
+        }
     }
 }
