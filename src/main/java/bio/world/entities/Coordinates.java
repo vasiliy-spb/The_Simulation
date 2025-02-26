c3ccdc3ac21b55f78c1549f798825bd4b4cfbd34
vasiliy-spb <vasiliy-spb07@mail.ru>
2025-02-27 03:15:52 +0700

    Made a refactor of Coordinates class

--- src/main/java/bio/world/entities/Coordinates.java
+++ src/main/java/bio/world/entities/Coordinates.java
@@ -1,20 +1,4 @@
 package bio.world.entities;
 
 public record Coordinates(int row, int column) {
-    public void print() {
-        System.out.printf("[%d - %d]%n", row, column);
-    }
-
-    public String getPosition() {
-        return "[%d - %d]%n".formatted(row, column);
-    }
-
-    @Override
-    public String toString() {
-        return "[%d - %d]".formatted(row, column);
-//        return "Coordinates{" +
-//                "row=" + row +
-//                ", column=" + column +
-//                '}';
-    }
 }
